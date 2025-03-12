2261406647182aa2edaa597ad160c4d39e9afeab
Ekwugha Franklin <ekwugha.franklin@gmail.com>
2025-03-12 23:09:41 +0100

    Second commit

---
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { json } from 'stream/consumers';
+import AuctionCard from './AuctionCard';
+import { Auction, PagedResult } from '../types';
+import AppPagination from '../components/AppPagination';
+
+async function getData(): Promise<PagedResult<Auction>>
+{
+  const res=await fetch('http://localhost:6001/search?pageSize=4');
+
+  if(!res.ok)
+     throw new Error('failed to fetch data') ;
+
+  return res.json();
+}
+
+export default async function Listings() {
+  const data =await getData();
+  return (
+    <>
+    <div data-ignore-grammarly className='grid grid-cols-4 gap-6'>
+      {data && data.results.map((auction:any) =>(
+        <AuctionCard auction={auction} key={auction.id}/>
+      ))}
+
+    </div>
+    <div className='flex justify-center mt-4'>
+      <AppPagination currentPage={1} pageCount={data.pageCount}></AppPagination>
+      </div>
+    </>
+    
+  )
+}
